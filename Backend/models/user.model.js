4469f983e26f77182e94d209431af5d2bcc67e15
Jatinmishra02 <mishrajatin877@gmail.com>
2026-03-04 17:43:54 +0530

    update models

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,30 +1,30 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    fullname:{
-        firstname:{
-            type:String,
-            required:true,
-            minlength:[3,'First name must be at least 3 characters long'],
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long'],
         },
-         lastname:{
-            type:String,
-            minlength:[3,'First name must be at least 3 characters long'],
+        lastname: {
+            type: String,
+            minlength: [3, 'First name must be at least 3 characters long'],
         }
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        minlength:[5,'email must be long']
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, 'email must be long']
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
-    socketId:{
-        type:String,
+    socketId: {
+        type: String,
 
     },
-    
+
 })
